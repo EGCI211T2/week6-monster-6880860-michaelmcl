a428df8280cdddb21aa8d6af4667b97a9aefbc68
6880860-michaelmcl <michael.mcl@student.mahidol.edu>
2026-02-11 04:31:52 +0000

    my first commit

--- monster.h
+++ monster.h
@@ -7,7 +7,30 @@ private:
 	int hp,potion;
 public:
 	void Attack(monster &);
-          void heal();
-	monster(string, int,int);
+    void heal();
+	monster(string="Unknown", int=1,int=1);
+	~monster();
+	void display();
 };
 #endif
+
+monster::monster(string n, int h, int p)
+{
+	name=n;
+	hp=h;
+	potion=p;
+	cout<<"Monster "<<name<<" is here"<<endl;
+}
+
+monster::~monster()
+{
+	cout<<name<<" is gone."<<endl;
+}
+
+void monster::display()
+{
+	cout<<"Name: "<<name<<endl;
+	cout<<"HP: "<<hp<<endl;
+	cout<<"Potions: "<<potion<<endl;
+}
+
